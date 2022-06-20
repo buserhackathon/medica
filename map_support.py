5611ad5e5e968c8bccf5a445601f6015597fb39a
assismauro <assismauro@hotmail.com>
2022-06-20 07:52:09 -0300

    Versão 0.9: mostra as UBSs filtradas por cidade, iniciando na capital do estado selecionado.

--- map_support.py
+++ map_support.py
@@ -7,10 +7,7 @@ import dbquery
 mapbox_access_token = 'pk.eyJ1IjoiYXNzaXNtYXVybyIsImEiOiJja3RvcGt2eTgwZXc5Mm9taGd6MTltZ2o2In0.FJ2GqIssNuJxeYh0ewTpLw'
 
 
-def geoWhere(uf: str, municipio_id: int = None) -> str:
-    if municipio_id == '0':
-        return f"where uf = '{uf}'"
-    else:
+def geoWhere(municipio_id: int = None) -> str:
         return f"where municipio_id = {municipio_id}"
 
 
@@ -26,36 +23,37 @@ order by capital desc, name""")
 
 
 def getUBSs(uf, municipio_id):
-    where = geoWhere(uf, municipio_id)
-    df = dbquery.getDataframeResultSet(f"select  id, nom_estab "
-                                       f"from ubs {where}")
+    # Se o município não corresponde à UF obtém a cidade correta, a capital
+    uf_municipio_id = dbquery.getValueFromDb(f"select state from municipio where id = {municipio_id}")
+    if uf_municipio_id != uf:
+        municipio_id = dbquery.getValueFromDb(f"select id from municipio where state = '{uf}' and capital = 1")
+    # ...
+    where = geoWhere(municipio_id)
+    df = dbquery.getDataframeResultSet(
+        f"select  id, nom_estab || '<br>' || dsc_endereco || '<br>' || dsc_bairro as endereco "
+        f"from ubs {where}")
+
     gjson = dbquery.getJSONResultset(
         f"select json_build_object('type', 'FeatureCollection','features', json_agg(ST_AsGeoJSON(t.*)::json)) "
-        f"from (select id, nom_estab, st_buffer(geom,0.001) as geometry from ubs {where}) "
-        "as t(id, nom_estab, geometry)")
-
-    df_mun = dbquery.getDataframeResultSet(f"select  id, name as nom_estab "
-                                       f"from municipio where state = 'AC' limit 1")
-
-    gjson_mun = dbquery.getJSONResultset(
-        f"select json_build_object('type', 'FeatureCollection','features', json_agg(ST_AsGeoJSON(t.*)::json)) "
-        f"from (select id, name as nom_estab, geom as geometry from municipio where state = 'AC' limit 1) "
-        "as t(id, nom_estab, geometry)")
+        f"from (select id, "
+        f"st_buffer(geom,0.0005) as geometry "
+        f"from ubs {where}) "
+        f"as t(id, geometry)")
+    #
+    # df_mun = dbquery.getDataframeResultSet(f"select  id, name as nom_estab "
+    #                                    f"from municipio where state = 'AC' limit 1")
+    #
+    # gjson_mun = dbquery.getJSONResultset(
+    #     f"select json_build_object('type', 'FeatureCollection','features', json_agg(ST_AsGeoJSON(t.*)::json)) "
+    #     f"from (select id, name as nom_estab, geom as geometry from municipio where state = 'AC' limit 1) "
+    #     "as t(id, nom_estab, geometry)")
 
     centroid_extent = dbquery.executeSQL(
-        f"""select st_x(st_centroid(buffer)) as long,st_y(st_centroid(buffer)) as lat, 
-       st_extent(buffer) 
-from 
-        (select st_buffer(ST_MakePolygon(ST_MakeLine(
- array[st_point(min(st_x(geom)), min(st_y(geom))),
-       st_point(max(st_x(geom)), min(st_y(geom))),
-       st_point(max(st_x(geom)), max(st_y(geom))),
-       st_point(min(st_x(geom)), max(st_y(geom))),
-       st_point(min(st_x(geom)), min(st_y(geom)))
-       ])), 0.06) as buffer     
-           from ubs
-           {where}) a
-group by buffer
+        f"""select st_x(m.centroide_area_urbana) as long,st_y(m.centroide_area_urbana) as lat, 
+       st_extent(m.geom) 
+from municipio m
+where id = {municipio_id}
+group by m.centroide_area_urbana
 """)[0]
 
     extent = centroid_extent[2]
@@ -63,12 +61,11 @@ group by buffer
         values = extent.replace(',', ' ').replace('(', ' ').replace(')', ' ').split(' ')
         max_bound = max(abs(float(values[1]) - float(values[3])),
                         abs(float(values[2]) - float(values[4]))) * 111  # km/degree
-        zoom = 17.5 - np.log(max_bound)
+        zoom = 19.5 - np.log(max_bound)
     else:
         zoom = 13.5
 
     return df, gjson, {"lat": centroid_extent[1], "lon": centroid_extent[0]}, zoom
-    #return df_mun, gjson_mun, {"lat": centroid_extent[1], "lon": centroid_extent[0]}, zoom
 
 
 def getFigUBSs(uf, municipio_id) -> str:
@@ -76,7 +73,7 @@ def getFigUBSs(uf, municipio_id) -> str:
     fig = px.choropleth_mapbox(ubss, geojson=geo, color=ubss.id,
                                locations=ubss.id, featureidkey="properties.id",
                                center=centroid,
-                               hover_name=ubss.nom_estab, hover_data={'id': False},
+                               hover_name=ubss.endereco, hover_data={'id': False},
                                mapbox_style="carto-positron", zoom=zoom)
     fig.update_layout(
         mapbox_style="white-bg",
